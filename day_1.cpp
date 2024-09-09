2d4135ecd05f760f43b6693ff11f82df18127bc7
DHARSHIKA S <116824635+dharshi4228@users.noreply.github.com>
2024-09-09 12:09:10 +0530

    day_1.cpp

--- day_1.cpp
+++ day_1.cpp
@@ -1,23 +1,47 @@
-#include<iostream>
+
+#include <iostream>
 using namespace std;
+void sort(int arr[], int s) {
+ int l= 0, mid = 0, h = s - 1;
+ 
+ while (mid <= h) {
+ if (arr[mid] == 0) {
+ 
+ swap(arr[l], arr[mid]);
+ l++;
+ mid++;
+ } else if (arr[mid] == 1) {
+ mid++;
+ 
+ } else if (arr[mid] == 2) {
+ swap(arr[mid], arr[h]);
+ h--;
+ }
+ }
+}
+
+void toprint(int arr[], int s) {
+    
+    for (int i =0 ; i<s ;i++) {
+        cout<<arr[i]<<" ";
+ }
+ cout << endl;
+}
+
 int main(){
-	int arr[] = {0, 1, 2, 1, 0, 2, 1, 0};
-	int s= size(arr);
-	for (int i=0 ; i<s-1 ;i++){
-		for (int j=0; j<s-i-1;j++){
-			if (arr [j] >arr[j+1]){
-				swap(arr[j],arr[j+1]);
-	        }
-	    }
-	}
-	
-	for(int i =0; i<s;i++){
-		cout<< arr[i];
-		if (i!= s-1){
-		    cout<<",";
-		}
-	}
-	cout<<endl;
+ 
+ int arr[] = {0, 1, 2, 1, 0, 2, 1, 0};
+ int n = sizeof(arr)/ sizeof(arr[0] );
+
+ cout << "Input: ";
+ toprint(arr, n);
+
+ sort(arr, n);
+
+ cout << "Output: ";
+ toprint(arr, n);
+
+
 }
 	
 		
