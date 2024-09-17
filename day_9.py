a2223263f04089194b81590327cf5b86f626decb
DHARSHIKA S <116824635+dharshi4228@users.noreply.github.com>
2024-09-18 00:21:37 +0530

    Add files via upload

---
+++ day_9.py
@@ -0,0 +1,9 @@
+ans=""
+        v.sort()
+        first=v[0]
+        last=v[-1]
+        for i in range(min(len(first),len(last))):
+            if(first[i]!=last[i]):
+                return ans
+            ans+=first[i]
+        return ans
